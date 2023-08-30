75394811079b432042d4035fa6fe5b247ac1aad1
sanmoyano <smoyano.di@gmail.com>
2023-08-30 17:14:01 -0300

    adding fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        primary: '#154d82',
+        secondary: '#ea3237',
+        white: '#f5f5f5'
       }
     }
   },
