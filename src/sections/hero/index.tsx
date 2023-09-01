0fdf761889ff6d24d48d1e8661b6d3023c8922dc
sanmoyano <smoyano.di@gmail.com>
2023-09-01 16:50:32 -0300

    update hero

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -4,15 +4,16 @@ import React from 'react'
 import HeroTitle from '@/components/common/title'
 import Button from '@/components/common/button'
 
-const HeroSection = () => {
+const HeroSection: React.FC = () => {
   return (
     <div className='bg-hero-section bg-cover bg-no-repeat bg-center bg-fixed w-full h-screen flex items-center justify-start'>
       <div className='flex flex-col justify-center w-full h-full bg-gradient-to-b from-[rgba(0,0,0,.5)] to-transparent'>
-        <div className='flex flex-col flex-nowrap h-fit pl-3.5 md:pl-6 lg:pl-12 w-[80%] md:w-[70%] lg:w-[50%]'>
+        <div className='flex flex-col flex-nowrap h-fit pl-3.5 md:pl-6 lg:pl-12 w-[80%] md:w-[70%] lg:w-[45%]'>
           <HeroTitle content='Acá arriba, la vista es' span=' mejor' />
           <div className='flex flex-row mt-10 space-x-2'>
             <Button content='Contactanos' onClick={() => console.log('contacto')} />
             <Button transparent content='Conoce más' icon=' →' onClick={() => console.log('mas')} />
+            p
           </div>
         </div>
       </div>
