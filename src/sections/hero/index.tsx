5ba4115cb46d6210389953433506ba36fcf49145
sanmoyano <smoyano.di@gmail.com>
2023-09-01 16:52:03 -0300

    update

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -9,11 +9,10 @@ const HeroSection: React.FC = () => {
     <div className='bg-hero-section bg-cover bg-no-repeat bg-center bg-fixed w-full h-screen flex items-center justify-start'>
       <div className='flex flex-col justify-center w-full h-full bg-gradient-to-b from-[rgba(0,0,0,.5)] to-transparent'>
         <div className='flex flex-col flex-nowrap h-fit pl-3.5 md:pl-6 lg:pl-12 w-[80%] md:w-[70%] lg:w-[45%]'>
-          <HeroTitle content='Acá arriba, la vista es' span=' mejor' />
+          <HeroTitle content='Acá arriba, la vista es' span=' mejor.' />
           <div className='flex flex-row mt-10 space-x-2'>
             <Button content='Contactanos' onClick={() => console.log('contacto')} />
             <Button transparent content='Conoce más' icon=' →' onClick={() => console.log('mas')} />
-            p
           </div>
         </div>
       </div>
