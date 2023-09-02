fefa50cd584e997dc496cc704f8714cf4634efe7
sanmoyano <smoyano.di@gmail.com>
2023-09-02 11:55:21 -0300

    logo with link

--- src/components/common/logo/index.tsx
+++ src/components/common/logo/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo: React.FC = () => {
   return (
     <div className='h-fit w-fit'>
-      <Image alt='Dycsa - Desarrolla, Comercializa y Entrega' height={50} src='/logo/dycsa-logo.svg' width={100} />
+      <Link href='/'>
+        <Image alt='Dycsa - Desarrolla, Comercializa y Entrega' height={50} src='/logo/dycsa-logo.svg' width={100} />
+      </Link>
     </div>
   )
 }
