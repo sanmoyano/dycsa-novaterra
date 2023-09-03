23a6e00d8f8d31087ec9cdb884627a624a04c683
sanmoyano <smoyano.di@gmail.com>
2023-09-03 15:59:55 -0300

    Tabs info

--- src/components/container/tabs/index.tsx
+++ src/components/container/tabs/index.tsx
@@ -12,7 +12,8 @@ export interface ITabsInfo {
   tab:string
   title:string
   description:string
-  img:StaticImageData
+  img:StaticImageData,
+  alt:string
 }
 
 const tabs: ITabsInfo[] = [
@@ -20,19 +21,22 @@ const tabs: ITabsInfo[] = [
     tab: 'Complejo',
     title: 'El complejo, el lugar en el vas a vivir',
     description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
-    img: complejoImg
+    img: complejoImg,
+    alt: 'Dycsa - Vista aera del complejo, cancha de tenis, playa de estacionamiento y pileta.'
   },
   {
     tab: 'Departamentos',
     title: 'Semipisos, A tu altura',
     description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
-    img: deptoImg
+    img: deptoImg,
+    alt: 'Dycsa - Interior del departamento, sala de estar o living vidreado con vista al estadio'
   },
   {
     tab: 'Amenities',
     title: 'Aprovechá al máximo las prestaciones',
     description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
-    img: amenitiesImg
+    img: amenitiesImg,
+    alt: 'Dycsa - Persona ejercitando en el gimnasio con vista a la pileta'
   }
 ]
 
