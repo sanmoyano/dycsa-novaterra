502098a9557216f1fcd4a3a45469fbf7c08d4bf3
sanmoyano <smoyano.di@gmail.com>
2023-09-03 15:59:35 -0300

    Button update

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 
 const Button = ({ content, onClick, transparent, icon, cssProps }: ButtonProps) => {
   return (
-    <button className={`${!transparent ? 'bg-secondary-100 hover:bg-secondary-200' : 'bg-transparent hover:bg-gray-200 hover:border-white'} text-white border border-transparent font-normal py-2 px-4 rounded transition-all duration-500 h-fit ${cssProps}`} onClick={onClick}>
+    <button className={`${!transparent ? 'bg-secondary-100 hover:bg-secondary-200' : 'bg-transparent hover:bg-gray-200 hover:border-white'} text-white border border-transparent font-normal py-2 px-4 rounded transition-all duration-500 h-fit w-fit ${cssProps}`} onClick={onClick}>
       <span>{content}</span>
       <span>{icon}</span>
     </button>
