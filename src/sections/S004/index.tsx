8686dfef3b991cdadb282ad578df0f5bc5466c90
sanmoyano <smoyano.di@gmail.com>
2023-09-07 11:06:14 -0300

    adding section 4

---
+++ src/sections/S004/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Section from '@/components/common/section'
+import VisibleContainer from '@/components/container/visibleContainer'
+
+const SectionCuatro = () => {
+  return (
+    <Section>
+      <VisibleContainer>
+        <div>SectionCuatro</div>
+      </VisibleContainer>
+    </Section>
+  )
+}
+
+export default SectionCuatro
