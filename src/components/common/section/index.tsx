3d42c3d92acd09084e5e18303172b1f479a7181d
sanmoyano <smoyano.di@gmail.com>
2023-09-07 11:07:03 -0300

    update app

--- src/components/common/section/index.tsx
+++ src/components/common/section/index.tsx
@@ -7,7 +7,7 @@ type SectionProps = {
 
 const Section: React.FC<SectionProps> = ({ children, id }) => {
   return (
-    <section className='p-3.5 md:p-8 lg:p-12 min-[1400px]:mx-12 min-[1500px]:mx-24 min-[1700px]:mx-48' id={id}>
+    <section className='px-3.5 py-8 md:p-12 lg:p-12 xl:p-24' id={id}>
       {children}
     </section>
   )
--- src/components/container/tabs/index.tsx
+++ src/components/container/tabs/index.tsx
@@ -8,35 +8,82 @@ import complejoImg from '../../../../public/images/tab-complejo-casa-grande.jpg'
 import deptoImg from '../../../../public/images/tab-depto-casa-grande.jpg'
 import amenitiesImg from '../../../../public/images/tab-amenities-casa-grande.jpg'
 
+type Dato = {
+  dato:string
+  number:number
+}
 export interface ITabsInfo {
   tab:string
   title:string
   description:string
   img:StaticImageData,
   alt:string
+  datos:Dato[]
 }
 
 const tabs: ITabsInfo[] = [
   {
     tab: 'Complejo',
     title: 'El complejo, el lugar en el que vas a vivir',
-    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
+    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit.',
     img: complejoImg,
-    alt: 'Dycsa - Vista aera del complejo, cancha de tenis, playa de estacionamiento y pileta.'
+    alt: 'Dycsa - Vista aera del complejo, cancha de tenis, playa de estacionamiento y pileta.',
+    datos: [
+      {
+        dato: 'PRIMER DATO',
+        number: 12
+      },
+      {
+        dato: 'SEGUNDO DATO',
+        number: 86
+      },
+      {
+        dato: 'TERCER DATO',
+        number: 123
+      }
+    ]
   },
   {
     tab: 'Departamentos',
     title: 'Semipisos, A tu altura',
-    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
+    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit.',
     img: deptoImg,
-    alt: 'Dycsa - Interior del departamento, sala de estar o living vidreado con vista al estadio'
+    alt: 'Dycsa - Interior del departamento, sala de estar o living vidreado con vista al estadio',
+    datos: [
+      {
+        dato: 'PRIMER DATO',
+        number: 12
+      },
+      {
+        dato: 'SEGUNDO DATO',
+        number: 86
+      },
+      {
+        dato: 'TERCER DATO',
+        number: 123
+      }
+    ]
   },
   {
     tab: 'Amenities',
     title: 'Aprovechá al máximo las prestaciones',
-    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit. In commodo eu aenean sem.',
+    description: 'Lorem ipsum dolor sit amet consectetur. Nullam velit cras molestie elit sem et gravida quis sodales. Nisl nunc odio pellentesque velit.',
     img: amenitiesImg,
-    alt: 'Dycsa - Persona ejercitando en el gimnasio con vista a la pileta'
+    alt: 'Dycsa - Persona ejercitando en el gimnasio con vista a la pileta',
+    datos: [
+      {
+        dato: 'PRIMER DATO',
+        number: 12
+      },
+      {
+        dato: 'SEGUNDO DATO',
+        number: 86
+      },
+      {
+        dato: 'TERCER DATO',
+        number: 123
+      }
+    ]
   }
 ]
 
