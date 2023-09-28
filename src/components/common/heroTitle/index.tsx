e5ccc7d778db1e8f766f710e3daea142f5a4b063
sanmoyano <smoyano.di@gmail.com>
2023-09-28 10:37:13 -0300

    [update] hero section

--- src/components/common/heroTitle/index.tsx
+++ src/components/common/heroTitle/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 type TitleProps = {
   content:string,
-  span:string
+  span?:string
   cssProps?:string
 }
 const HeroTitle: React.FC<TitleProps> = ({ content, span, cssProps }) => {
   return (
-    <h1 className={`font-normal text-white text-6xl lg:text-7xl xl:text-8xl w-fit ${cssProps}`}>
+    <h1 className={`font-bold text-white text-6xl lg:text-7xl xl:text-7xl w-fit ${cssProps}`}>
       {content}
-      <span className='font-bold'>{span}</span>
+      {/* <span className='font-bold'>{span}</span> */}
     </h1>
   )
 }
