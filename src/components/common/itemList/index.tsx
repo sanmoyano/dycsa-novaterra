7740e7835f710fe8297b1130590a244c7d7479e6
sanmoyano <smoyano.di@gmail.com>
2023-10-09 11:12:56 -0300

    [update] text item

--- src/components/common/itemList/index.tsx
+++ src/components/common/itemList/index.tsx
@@ -9,8 +9,8 @@ type InfoProps = {
 
 const ItemList: React.FC<InfoProps> = ({ info, cssProps, icon, stroke }) => {
   return (
-    <li className={`flex flex-row ${cssProps} w-full items-center space-x-1`}>
-      <svg className='w-8 h-8' fill='none' stroke='currentColor' strokeWidth={1.5} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+    <li className={`flex flex-row ${cssProps} w-1/2 items-center space-x-4`}>
+      <svg className='w-10 h-10' fill='none' stroke='currentColor' strokeWidth={1.5} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
         {icon}
       </svg>
       <p className='text-xs xl:text-lg w-fit'>{info}</p>
