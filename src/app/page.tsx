3d1c675ec507f1cd402d07af8b293b7728333106
sanmoyano <smoyano.di@gmail.com>
2023-12-03 21:59:12 -0300

    [update] section 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home () {
         <SectionUno />
         <SectionDos />
         <SectionTres />
-        <SectionCuatro />
+        {/* <SectionCuatro /> */}
         <SectionCinco />
       </SectionsLayout>
     </main>
