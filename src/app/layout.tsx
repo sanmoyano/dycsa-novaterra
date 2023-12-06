ba1deea7df9ac078157cf8e084a69cb2c81b016d
sanmoyano <smoyano.di@gmail.com>
2023-12-06 20:44:04 -0300

    [add] analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,21 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
         <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
         <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
         <link href='/site.webmanifest' rel='manifest' />
+        <Script async src='https://www.googletagmanager.com/gtm.js?id=GTM-M8DVVCJ' />
+        <Script async id='tag'>
+          {`
+          
+            window.dataLayer = [{"title":"Novaterra III","author":"Control","wordcount":2498,"logged_in":"false","page_id":6698,"post_date":"2019-01-15 03:32:51"}];
+
+          `}
+        </Script>
+        <Script async id='google'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M8DVVCJ')`}
+        </Script>
       </head>
       <body className={`${pangram.variable}`}>
         <Header />
